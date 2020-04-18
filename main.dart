e8551560081123c7a34317ff26244133438c9366
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-04-18 17:19:39 +0200

    basic functionality

--- main.dart
+++ main.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
-import './Pages/Home.dart';
+import 'package:kartofgram/Pages/HomePage.dart';
+import 'package:kartofgram/Pages/NotificationsPage.dart';
+import 'package:kartofgram/Pages/ProfilePage.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(
       MaterialApp(
+        title: "Kartofgram",
+        theme: ThemeData(primarySwatch: Colors.blue),
         home: App(),
+        
         //color: Colors.black,
       ),
     );
 
 class App extends StatelessWidget {
-  final List<Widget> myPages = <Widget>[
-    Home(),
+  final List<Widget> myPages = [
+    HomePage(),
+    NotificationsPage(),
+    ProfilePage(),
   ];
 
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
+    //SystemChrome.setEnabledSystemUIOverlays ([SystemUiOverlay.top]);
     return DefaultTabController(
         initialIndex: 0,
-        length: 5,
+        length: 3,
         child: Scaffold(
-          backgroundColor: Colors.white,
-        ));
+            resizeToAvoidBottomPadding: false,
+            body: TabBarView(
+              children: myPages,
+            ),
+            bottomNavigationBar: Container(
+              margin: EdgeInsets.only(bottom: 1),
+              color: Colors.black87,
+              child: new TabBar(tabs: [
+                Tab(
+                  icon: Icon(Icons.home),
+                ),
+                Tab(
+                  icon: Icon(Icons.account_circle),
+                ),
+                Tab(
+                  icon: Icon(Icons.notifications),
+                )
+              ]),
+            )));
   }
 }
