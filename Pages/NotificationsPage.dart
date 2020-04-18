4d5e12d5080446113b9e0660508adbc55598f8c6
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-04-18 17:20:13 +0200

    basic functionality

---
+++ Pages/NotificationsPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationsPage extends StatefulWidget{
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage>{
+  @override
+  Widget build(BuildContext context){
+    return Container(color: Colors.yellow,);
+  }
+}
